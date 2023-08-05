9dc3277b47685ff8e613f61e1f88b1708667a3e5
CristianSenatore <crsenatore.cs@gmail.com>
2023-08-05 06:27:08 +0200

    Nuevos cambios

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Autos)
+admin.site.register(Clientes)
+admin.site.register(Vendedores)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -7,14 +7,23 @@ class Autos(models.Model):
     anio = models.IntegerField(null=False, blank=False )
     color = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.marca},{self.version}"
+
 class Clientes(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     dni = models.IntegerField()
     mail = models.EmailField()
+    
+    def __str__(self):
+        return f"{self.nombre},{self.apellido}"
 
 class Vendedores(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     legajo = models.IntegerField()
     mail = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre},{self.apellido}"
